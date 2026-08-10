3c8ef94a2a6bfee7cd8ae353af9c0f70793e463a
Mike <142720633+Mikequez12@users.noreply.github.com>
2026-08-10 16:36:34 +0200

    Modify socket.getaddrinfo to use IPv4
    Override socket.getaddrinfo to use IPv4 only.

--- spam.py
+++ spam.py
@@ -3,6 +3,14 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import sys
 import json
+import socket
+
+original_getaddrinfo = socket.getaddrinfo
+
+def getaddrinfo_ipv4(host, port, family=0, type=0, proto=0, flags=0):
+    return original_getaddrinfo(host, port, socket.AF_INET, type, proto, flags)
+
+socket.getaddrinfo = getaddrinfo_ipv4
 
 weburl = "http://base-de-noviercas.onrender.com"
 
